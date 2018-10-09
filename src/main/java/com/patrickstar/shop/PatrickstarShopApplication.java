820fb4b01f5e077c7ca75c931816c43d721c29f7
Seonghun Kang <lutis107@gmail.com>
2018-10-09 15:53:29 +0900

    Initial commit Create project using Spring Starter Project

---
+++ src/main/java/com/patrickstar/shop/PatrickstarShopApplication.java
@@ -0,0 +1,12 @@
+package com.patrickstar.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatrickstarShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatrickstarShopApplication.class, args);
+	}
+}
